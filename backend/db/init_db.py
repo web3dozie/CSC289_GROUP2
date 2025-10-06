7bb113dce5511d36ecafc22138dc88547000254c
John Diveris <jdiveris@my.waketech.edu>
2025-10-05 22:10:27 -0400

    chore[backend]: remove old init.py file

--- backend/db/init_db.py
+++
@@ -1,8 +0,0 @@
-from .models import Base
-from .engine_async import async_engine
-
-
-# Create db tables
-async def init_models():
-    async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
