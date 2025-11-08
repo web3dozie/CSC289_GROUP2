d821adbc1084d5106488ffd56d5c362b84579536
John Diveris <jdiveris@my.waketech.edu>
2025-11-08 18:07:14 -0500

    chore[backend]: Remove old sessions.py file

--- backend/db/session.py
+++
@@ -1,8 +0,0 @@
-from contextlib import asynccontextmanager
-from .engine_async import AsyncSessionLocal
-
-
-@asynccontextmanager
-async def get_session():
-    async with AsyncSessionLocal() as session:
-        yield session
